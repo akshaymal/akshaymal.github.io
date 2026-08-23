c09308c338e528d77134eabf2d90f45cabd44617
Akshay Malhotra <malhotraakshay1997@gmail.com>
2026-08-22 22:12:57 -0400

    Add Beyond Work page (placeholder content) and remove old hobby routes

--- app/hobbies/motorsports/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Formula1Page() {
-  return (
-    <div>
-      <p>This is formula 1 page</p>
-    </div>
-  )
-}
--- app/hobbies/sim-racing/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function SimRacingPage() {
-  return (
-    <div>
-      <p>This is sim racing page</p>
-    </div>
-  )
-}
